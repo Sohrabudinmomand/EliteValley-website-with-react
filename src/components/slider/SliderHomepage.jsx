836833d94a070e12df774dc5662852cf995a1920
Sohrab momand <sohrabmomand.work@gmail.com>
2023-12-18 19:49:56 +0430

    fixing the error why the css is not working

--- src/components/slider/SliderHomepage.jsx
+++ src/components/slider/SliderHomepage.jsx
@@ -7,25 +7,6 @@ import FristImage from "./assets/1-image-for-slider.png";
 import LogoInfirstSlider from "./assets/logo-in-first-slider.png";
 
 const SliderHomepage = () => {
-  const slideContents = [
-    {
-      title: "App design",
-      image: FristImage,
-      logo: LogoInfirstSlider,
-      description: `Discover powerful features to boost your productivity. You are always welcome to visit our little den. Professional in their link.`,
-      clientName: "Khalid Atayee",
-      budget: "$500-100K",
-    },
-    {
-      title: "Web design",
-      image: FristImage,
-      logo: LogoInfirstSlider,
-      description: `Explore the latest trends in web design. Our team will bring your vision to life with creativity and precision.`,
-      clientName: "Sara Johnson",
-      budget: "$500-100K",
-    },
-  ];
-
   const settings = {
     dots: true,
     infinite: true,
@@ -38,7 +19,7 @@ const SliderHomepage = () => {
 
   return (
     <div className="container-slider-section-home-page">
-      <div className="{styles.content-slider-section-home-page}">
+      <div className="content-slider-section-home-page">
         <div className="description-box-slider-home-page">
           <h1 className="title-slider-box-home-page">
             Check out some of our recent work.
@@ -46,34 +27,38 @@ const SliderHomepage = () => {
         </div>
         <div className="sliding-boxes-slider-home-page">
           <Slider {...settings} className="custom-slider">
-            {slideContents.map((slide, index) => (
-              <div key={index} className="first-slider-box">
+            <div className="first-slider-box">
+              <img
+                className="image-content-for-slider"
+                src={FristImage}
+                alt="First for Slider"
+              />
+              <div className="content-slider">
                 <img
-                  src={slide.image}
-                  alt="slide"
-                  className="image-content-for-slider"
+                  className="logo-slider"
+                  src={LogoInfirstSlider}
+                  alt="Logo"
                 />
-                <div className="content-slider">
-                  <img src={slide.logo} alt="logo" className="logo-slider" />
-                  <h2 className="title-slider">{slide.title}</h2>
-                  <p className="paragraph-slider">{slide.description}</p>
-                  <div className="sub-content-slider-box">
-                    <div className="customer-info-box">
-                      <div>
-                        <div className="client-title">Client Name</div>
-                        <div className="client-paragraph">
-                          {slide.clientName}
-                        </div>
-                      </div>
-                      <div>
-                        <div className="client-title">Budget</div>
-                        <div className="client-paragraph">{slide.budget}</div>
-                      </div>
+                <h1 className="title-slider">App Design</h1>
+                <p className="paragraph-slider">
+                  Discover powerful features to boost your productivity. You are
+                  always welcome to visit our little den. Professional in their
+                  link.
+                </p>
+                <div className="sub-content-slider-box">
+                  <div className="customer-info-box">
+                    <div>
+                      <div className="client-title">Client Name</div>
+                      <div className="client-paragraph">Khalid Atayee</div>
+                    </div>
+                    <div>
+                      <div className="client-title">Budget</div>
+                      <div className="client-paragraph">$500-100k</div>
                     </div>
                   </div>
                 </div>
               </div>
-            ))}
+            </div>
           </Slider>
         </div>
       </div>
