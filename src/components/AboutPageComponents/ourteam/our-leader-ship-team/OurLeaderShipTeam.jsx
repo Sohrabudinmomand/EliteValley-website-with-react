13bc0e19113ef17e13086499208481ae0dfe7847
Sohrab momand <sohrabmomand.work@gmail.com>
2024-01-06 16:38:04 +0430

    our leader ship team is done

--- src/components/AboutPageComponents/ourteam/our-leader-ship-team/OurLeaderShipTeam.jsx
+++ src/components/AboutPageComponents/ourteam/our-leader-ship-team/OurLeaderShipTeam.jsx
@@ -1,8 +1,72 @@
 import React from "react";
-
+import FirstMemberImage from "./assets/first-member-img.jpeg";
+import SecondMemberImage from "./assets/second-member-img.png";
+import ThirdMemberImage from "./assets/third-member-img.jpeg";
+import FourthMemberImage from "./assets/fourth-member-img.png";
 import "./ourleadershipteam.css";
 
 const OurLeaderShipTeam = () => {
+  const membersDataOne = [
+    {
+      img: FirstMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: SecondMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: ThirdMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: FourthMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+  ];
+
+  const membersDataTwo = [
+    {
+      img: FirstMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: FirstMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: FirstMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+    {
+      img: FirstMemberImage,
+      name: "Mohammad Jan naser",
+      position: "CEO",
+      description:
+        "Ipsum architecto enim tempore ut voluptas repudiandae ab tempora.",
+    },
+  ];
   return (
     <div className="container-our-leader-shipteam-about-page">
       <div className="content-our-leader-shipteam-about-page">
@@ -16,9 +80,29 @@ const OurLeaderShipTeam = () => {
         </div>
         <div className="team-members-info-box">
           <div className="first-row-info-box">
-            <div className="member-info-box"></div>
+            {membersDataOne.map((item, index) => (
+              <div key={index} className="member-info-box">
+                <img className="img-member" src={item.img} alt="" />
+                <span className="member-name">{item.name}</span>
+                <span className="member-position">{item.position}</span>
+                <span className="description-about-member">
+                  {item.description}
+                </span>
+              </div>
+            ))}
           </div>
-          <div className="second-row-info-box"></div>
+          <div className="second-row-info-box">
+            {membersDataTwo.map((item, index) => (
+              <div key={index} className="member-info-box">
+                <img className="img-member" src={item.img} alt="" />
+                <span className="member-name">{item.name}</span>
+                <span className="member-position">{item.position}</span>
+                <span className="description-about-member">
+                  {item.description}
+                </span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
