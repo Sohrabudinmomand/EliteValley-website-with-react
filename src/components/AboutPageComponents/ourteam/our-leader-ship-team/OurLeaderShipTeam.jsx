40ca10c301936694ddfa3b393c62e02487d1aa70
Sohrab momand <sohrabmomand.work@gmail.com>
2024-01-06 12:46:55 +0430

    leader ship team member start

--- src/components/AboutPageComponents/ourteam/our-leader-ship-team/OurLeaderShipTeam.jsx
+++ src/components/AboutPageComponents/ourteam/our-leader-ship-team/OurLeaderShipTeam.jsx
@@ -5,7 +5,22 @@ import "./ourleadershipteam.css";
 const OurLeaderShipTeam = () => {
   return (
     <div className="container-our-leader-shipteam-about-page">
-      <div className="content-our-leader-shipteam-about-page"></div>
+      <div className="content-our-leader-shipteam-about-page">
+        <div className="description-box-our-leader-shipteam-about-page">
+          <span className="content-our-leadership-team">
+            Our leadership team
+          </span>
+          <h1 className="title-our-leadership-team">
+            Meet the amazing team behind Iori
+          </h1>
+        </div>
+        <div className="team-members-info-box">
+          <div className="first-row-info-box">
+            <div className="member-info-box"></div>
+          </div>
+          <div className="second-row-info-box"></div>
+        </div>
+      </div>
     </div>
   );
 };
